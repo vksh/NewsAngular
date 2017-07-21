8af51959636ae4bbcbf5c219b7399e8cc82e371f
Vikash Kumar 8 <vku155@sapient.com>
2017-07-21 16:07:35 +0530

    search player

--- app/route.js
+++ app/route.js
@@ -1,36 +1,35 @@
-   var app=angular.module('myApp',['ui.router'])
-     .config(function ($stateProvider,$urlServiceProvider) {
-         
-        $urlServiceProvider.rules.otherwise({state:'home'}); 
+var app = angular.module('myApp', ['ui.router'])
+    .config(function ($stateProvider, $urlServiceProvider) {
+
+        $urlServiceProvider.rules.otherwise({ state: 'home' });
         $stateProvider
             .state('home', {
-                url:'/',
-                //template:'<drafttable-component tabletype="draft"></table-component>'
-                template: '<header-component></header-component>'+
- '<carousel-component></carousel-component>'+
+                url: '/',
+                template: '<header-component></header-component>' +
+                '<carousel-component></carousel-component>' +
                 '<sports-component></sports-component>'
             })
-            .state('sports',{
-                url:'/sports',
-                template: '<header-component></header-component>'+
- '<carousel-component></carousel-component>'+
+            .state('sports', {
+                url: '/sports',
+                template: '<header-component></header-component>' +
+                '<carousel-component></carousel-component>' +
                 '<sports-component></sports-component> '
             })
-            .state('news',{
-                url:'/news',
-                template: '<header-component></header-component>'+
- '<carousel-component></carousel-component>'+
+            .state('news', {
+                url: '/news',
+                template: '<header-component></header-component>' +
+                '<carousel-component></carousel-component>' +
                 '<news-component></news-component>'
             })
 
-            .state('ranking',{
-                url:'/ranking',
-                template: '<header-component></header-component>'+
- '<carousel-component></carousel-component>'+
+            .state('ranking', {
+                url: '/ranking',
+                template: '<header-component></header-component>' +
+                '<carousel-component></carousel-component>' +
                 '<ranking-component></ranking-component>'
             })
-            .state('details',{
-                url:'/teamdetails',
+            .state('details', {
+                url: '/teamdetails',
                 template: '<teamdetails-component></teamdetails-component>'
             })
     })
